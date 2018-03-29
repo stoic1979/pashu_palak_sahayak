c6309795e0d9042f4d5d0cff11c3c7c0d0e3783c
Navjot Singh <rani.sonia768@gmail.com>
2018-03-29 18:25:35 +0530

    fixed disallowedhost issue

--- pashu_palak_api/settings.py
+++ pashu_palak_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3@3eos#d*)21oke3#b-wxrd&hxfsw1)oc26h##ckx=n339l+1x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
